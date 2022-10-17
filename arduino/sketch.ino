292afc1c93385ac3416fd666a15a4ae419e61caf
Patryk Zych <zych.patryk@outlook.com>
2022-10-17 19:43:30 +0200

    wifi

--- arduino/sketch.ino
+++ arduino/sketch.ino
@@ -5,8 +5,8 @@
 
 DHT dht(5, DHT11);
 
-const char* ssid = "komster.net-4jtTvoAx"; // CGA2121_fnwWXP9
-const char* password = "ihubea737NGR841s"; // PNfCJ5xV7qY8eSA9Ag
+const char* ssid = "test";
+const char* password = "test";
 const char* host = "192.168.0.1:5000";
 
 //WiFiServer server(80);
