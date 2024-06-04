db504a3b392aeb8d2f582986172c5e91f45b1ffb
zychpatryk <zych.patryk@outlook.com>
2024-06-04 19:15:03 +0200

    bazy danych

--- website/models.py
+++ website/models.py
@@ -16,9 +16,35 @@ class User(db.Model, UserMixin):
     active_participation = db.Column(db.Integer)
     from_where = db.Column(db.Integer)
     interests = db.Column(db.String(150))
-    
 
-class Temperature(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    temperature = db.Column(db.String(150))
+class Organisers(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    role = db.Column(db.Integer)
+    dep = db.Column(db.Integer)
 
+class Payments(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    autopay_val = db.Column(db.String(150))
+    state = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime)
+
+class Workshops(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    workshop_1 = db.Column(db.Integer)
+    workshop_2 = db.Column(db.Integer)
+    workshop_3 = db.Column(db.Integer)
+    workshop_4 = db.Column(db.Integer)
+    ergo_workshop = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime)
+
+class Participations(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    file_status = db.Column(db.Integer)
+    block = db.Column(db.Integer)
+    link_to_file = db.Column(db.String(150))
+
+class Tickets(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.ticket'), primary_key = True)
+    type_of = db.Column(db.String(150))
+    price = db.Column(db.Float(2))
+    currency = db.Column(db.String(150))
